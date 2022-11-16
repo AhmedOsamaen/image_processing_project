2a4c66384da86f96758ba7f797c218577ca3c893
Ahmed Osama <ahmed.osama@datagearbi.com>
2022-11-16 23:17:13 +0200

    Add image resizing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
 import sharp from'sharp'
 import express from 'express' 
-
+import fs from 'fs';
+import fsPromise from 'fs/promises'
 const app = express();
 const port = 3000;
+const mainImgsDir='./assets/imgs'
+const processedImgsDir='./assets/loaded-imgs/'
+const accessImgsDir="assets\\loaded-imgs\\"
  
 app.listen(port,()=>{
     console.log('The server started on port::>> ', port);
 });
 
-app.get('/convert', function (req, res:any) {
-    res.send( `Hello  ${req.query.imgName}, ${req.query.width},   ${req.query.height}`)
+app.get('/convert', async function (req, res) {
+    const imgWidth = req.query.width as string;
+    const imgHeight = req.query.height as string;
+    const imgName = req.query.imgName as string;
+    const savedFileName = processedImgsDir+imgName+'_'+imgWidth+'_'+imgHeight+'.jpg'
+    const accessFile= accessImgsDir+imgName+'_'+imgWidth+'_'+imgHeight+'.jpg'
+    let assetsDirectoryName;
+    if(__dirname.indexOf('src')!=-1){
+         assetsDirectoryName= __dirname.replace('src','')+accessFile;
+    }else{
+        assetsDirectoryName= __dirname.replace('dist','')+accessFile;
+    }
+    if(fs.existsSync(savedFileName)){
+        console.log('assetsDirectoryName :>> ', assetsDirectoryName);
+        res.type('jpg').sendFile(assetsDirectoryName)
+        return;
+    }
+    processImg(imgName,+imgWidth,+imgHeight);
+    const img=mainImgsDir+'/fjord.jpg';
+    await sharp(img).resize({ width: +imgWidth,height:+imgHeight }).toBuffer()
+    .then(data => {
+        res.type('jpg').send(data)
+    }).catch((e)=>{
+        console.log('e :>> ', e); 
+        return e;
+    });
     
 })
+
+function  processImg(fileName:string,imgWidth:number,imgHeight:number): void{
+    const img=mainImgsDir+'/fjord.jpg';
+    const savedFileName = processedImgsDir+fileName+'_'+imgWidth+'_'+imgHeight+'.jpg'
+   sharp(img).resize({ width: imgWidth,height:imgHeight }).toFile(savedFileName);
+   
+}
+
+// processImg(req.query.imgName as string,+imgWidth,+imgHeight).then((ress)=>{
+//     ress.toBuffer().then(data=>{
+//         res.send(res.type('jpg').send(data))
+//     })
+// })
